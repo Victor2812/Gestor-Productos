4b505b6526cef96ae7547e036e0eb649a9c81331
gorkauriarte <gorka.uriarte@ikasle.egibide.org>
2023-03-08 08:54:46 +0100

    c

--- resources/views/personas/index.blade.php
+++ resources/views/personas/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header">Manage Users</div>
+            <div class="card-header">Manage Personas</div>
             <div class="card-body">
                 {{!! $dataTable->table() !!}}
             </div>
